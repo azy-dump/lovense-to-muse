9a17a9434e2fe79a480c6e4b4aa53069d2ed21c5
azy-dump <azelynnperson@gmail.com>
2025-05-12 21:11:56 +0800

    implement callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,75 @@
 #define LOVENSE_NAME "LVS-S001"
 
 
+static BLECharacteristic *pCharRx = nullptr;
+static BLECharacteristic *pCharTx = nullptr;
+
+
+class ServerCallback : public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) override {
+        Serial.println("A device connected to the Lovense interface.");
+    }
+
+    void onDisconnect(BLEServer *pServer) override {
+        Serial.println("A device disconnected from the Lovense interface.");
+    }
+};
+
+
+class CharCallback : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pChar) {
+        if (pChar != pCharTx) {
+            Serial.println("Wrong characteristic.");
+            return;
+        }
+
+        std::string input = pChar->getValue();
+        Serial.println(input.c_str());
+
+        if (input == "DeviceType;") {
+            Serial.println("$Responding to Device Enquiry");
+            std::string wawa = "S:11:004B123A2E62;";
+            pCharRx->setValue(wawa);
+            pCharRx->notify();
+        } else {
+            Serial.println("$Wawa");
+            std::string wawa = "ERR;";
+            pCharRx->setValue(wawa);
+            pCharRx->notify();
+        }
+    }
+};
+
+
 void setup() {
     Serial.begin(115200);
     while (!Serial) delay(100);
 
 
     Serial.println("Initializing BLE...");
+
+
     BLEDevice::init(LOVENSE_NAME);
+
     BLEServer *pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallback());
+
     BLEService *pService = pServer->createService(LOVENSE_SERVICE_UUID);
 
 
-    BLECharacteristic *pCharTx = pService->createCharacteristic(
-        LOVENSE_CHAR_TX_UUID,
-        BLECharacteristic::PROPERTY_WRITE
-    );
-    // pCharacteristicTX->setCallbacks(new LovenseCallbacks());
-
-    BLECharacteristic *pCharRx = pService->createCharacteristic(
+    pCharRx = pService->createCharacteristic(
         LOVENSE_CHAR_RX_UUID,
         BLECharacteristic::PROPERTY_NOTIFY
     );
     pCharRx->addDescriptor(new BLE2902());
-
+    pCharRx->setCallbacks(new CharCallback());
+  
+    pCharTx = pService->createCharacteristic(
+        LOVENSE_CHAR_TX_UUID,
+        BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_WRITE_NR
+    );
+    pCharTx->setCallbacks(new CharCallback());
+    
 
 
     pService->start();
@@ -42,10 +88,10 @@ void setup() {
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
     pAdvertising->addServiceUUID(LOVENSE_SERVICE_UUID);
     pAdvertising->setScanResponse(true);
-    pAdvertising->setMinPreferred(0x06);
-    pAdvertising->setMinPreferred(0x12);
+    pAdvertising->setMinPreferred(0x0);
+    // pAdvertising->setMinPreferred(0x12);
     BLEDevice::startAdvertising();
-    Serial.println("Characteristic defined! Now you can read it in your phone!");
+    Serial.println("Initialized. Waiting for connection.");
 }
 
 void loop() {}
