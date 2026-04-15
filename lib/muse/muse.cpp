26669d8a1cddd0356f089cc21a97947e0e29f18e
azy-dump <azelynnperson@gmail.com>
2026-04-15 22:02:19 +0800

    Use better data list

--- lib/muse/muse.cpp
+++ lib/muse/muse.cpp
@@ -15,30 +15,19 @@ namespace Muse {
 
 
     uint8_t mf_data_list[][MUSE_MF_DATA_LENGTH] = {
-        // Stop all channels
-        {MUSE_MF_DATA_PREFIX, 0xE5, 0x15, 0x7D},
-        // Set all channels to speed 1
-        {MUSE_MF_DATA_PREFIX, 0xE4, 0x9C, 0x6C},
-        // Set all channels to speed 2
-        {MUSE_MF_DATA_PREFIX, 0xE7, 0x07, 0x5E},
-        // Set all channels to speed 3
-        {MUSE_MF_DATA_PREFIX, 0xE6, 0x8E, 0x4F},
-        // Stop 1st channel (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xD5, 0x96, 0x4C},
-        // Set 1st channel to speed 1 (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xD4, 0x1F, 0x5D},
-        // Set 1st channel to speed 2 (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xD7, 0x84, 0x6F},
-        // Set 1st channel to speed 3 (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xD6, 0x0D, 0x7E},
-        // Stop 2nd channel (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xA5, 0x11, 0x3F},
-        // Set 2nd channel to speed 1 (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xA4, 0x98, 0x2E},
-        // Set 2nd channel to speed 2 (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xA7, 0x03, 0x1C},
-        // Set 2nd channel to speed 3 (only for toys with 2 channels)
-        {MUSE_MF_DATA_PREFIX, 0xA6, 0x8A, 0x0D},
+        // Stop
+        {MUSE_MF_DATA_PREFIX, 0xe5, 0x0, 0x0},
+
+        // 1 - 9
+        {MUSE_MF_DATA_PREFIX, 0xf4, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xf7, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xf6, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xf1, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xf0, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xf3, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xe7, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xfc, 0x0, 0x0},
+        {MUSE_MF_DATA_PREFIX, 0xe6, 0x0, 0x0},
     };
 
 
@@ -54,7 +43,7 @@ namespace Muse {
     int current_lovense_intensity = 0;
 
     int lovense_intensity_max = 20;
-    int muse_intensity_max = 3;
+    int muse_intensity_max = 9;
 
 
     void set_manufacturer_data(uint8_t index) {
